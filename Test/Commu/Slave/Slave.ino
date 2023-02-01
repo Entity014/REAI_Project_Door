174bb8f5e91627421967cfb86061b48f4f693f44
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-02-02 00:30:59 +0700

    Add files via upload

--- Test/Commu/Slave/Slave.ino
+++ Test/Commu/Slave/Slave.ino
@@ -8,7 +8,7 @@
 
 Servo myservo;
 
-int i = 0;
+int stateDoor = 0, stateEB = 0;
 byte dataArray[8];
 String payload[(sizeof(dataArray)/sizeof(dataArray[0]))/2];
 
@@ -34,78 +34,85 @@ void receiveEvent(int bytes) {
   }
 }
 
-void open()
-{
-  myservo.write(0);
-  i = 1;
-}
-
-void close()
-{
-  myservo.write(180);
-  i = 0;
-}
-
 void loop() {
-  if(i == 0)
+  if(stateEB == 0)
   {
-    if(payload[0] == "B1" && payload[2] == "T0")
+    if(stateDoor == 0)
     {
-      digitalWrite(LED1, HIGH);
-      myservo.write(0);
-      i = 1;
-      delay(5000);
+      if(payload[0] == "B1" && payload[2] == "T0")
+      {
+        digitalWrite(LED1, HIGH);
+        myservo.write(0);
+        stateDoor = 1;
+        delay(5000);
+      }
+      if(payload[0] == "B1" && payload[2] == "T1")
+      {
+        digitalWrite(LED1, HIGH);
+        myservo.write(0);
+        stateDoor = 1;
+        delay(2500);
+      }
+      if(payload[3] == "W1")
+      {
+        myservo.write(0);
+        stateDoor = 1;
+        delay(5000);
+      }
     }
-    if(payload[0] == "B1" && payload[2] == "T1")
+    else if(stateDoor == 1)
     {
-      digitalWrite(LED1, HIGH);
-      myservo.write(0);
-      i = 1;
-      delay(2500);
+      if(payload[0] == "B1" && payload[2] == "T0")
+      {
+        digitalWrite(LED2, HIGH);
+        myservo.write(180);
+        stateDoor = 0;
+        delay(5000);
+      }
+      if(payload[0] == "B1" && payload[2] == "T1")
+      {
+        digitalWrite(LED2, HIGH);
+        myservo.write(180);
+        stateDoor = 0;
+        delay(2500);
+      }
+      if(payload[3] == "W1")
+      {
+        digitalWrite(LED4, HIGH);
+        delay(100);
+        digitalWrite(LED4, LOW);
+        delay(100);
+      }
     }
-    if(payload[3] == "W1")
+    if (payload[0] == "L1")
     {
-      myservo.write(0);
-      i = 1;
-      delay(5000);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      myservo.write(92);
+    }
+    if(payload[2] == "T0")
+    {
+      digitalWrite(LED3, LOW);
+    }
+    if (payload[2] == "T1")
+    {
+      digitalWrite(LED3, HIGH);
+    }
+    if (payload[1] == "B2")
+    {
+      stateEB = 1;
     }
   }
-  else if(i == 1)
-  {
-    if(payload[0] == "B1" && payload[2] == "T0")
-    {
-      digitalWrite(LED2, HIGH);
-      myservo.write(180);
-      i = 0;
-      delay(5000);
-    }
-    if(payload[0] == "B1" && payload[2] == "T1")
-    {
-      digitalWrite(LED2, HIGH);
-      myservo.write(180);
-      i = 0;
-      delay(2500);
-    }
-    if(payload[3] == "W1")
-    {
-      digitalWrite(LED4, HIGH);
-      delay(100);
-      digitalWrite(LED4, LOW);
-      delay(100);
-    }
-  }
-  if (payload[0] == "L1")
+  else if(stateEB == 1)
   {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
-    myservo.write(92);
-  }
-  if(payload[2] == "T0")
-  {
     digitalWrite(LED3, LOW);
-  }
-  if (payload[2] == "T1")
-  {
-    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, LOW);
+    myservo.write(92);
+    if (payload[1] == "B2")
+    {
+      stateEB = 0;
+    }
   }
 }
