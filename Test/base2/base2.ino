490ba3ff1cdd73821de9a50d34c6a8919c263b96
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-02-04 17:17:03 +0700

    Update base2.ino

--- Test/base2/base2.ino
+++ Test/base2/base2.ino
@@ -6,14 +6,17 @@
 #define buttonPin2 7
 #define tricPin 9
 #define echoPin 8
+#define buzzerPin 6
 
 Servo myservo;
 long duration, distance;
 unsigned long previousTLED = millis();
 unsigned long previousTServo = millis();
+unsigned long previousTBuzzer = millis();
 long timeIntervalLED = 100;
 long timeIntervalServoD = 5000;
-int ledState = 0, servoState = 90, stateDoor = 0;
+long timeIntervalBuzzer = 1000;
+int ledState = 0, servoState = 90, stateDoor = 0, stateB = 0, buzzerState = 0;
 
 void WaterFlood(unsigned long currentTime) {
   if (currentTime - previousTLED > timeIntervalLED) {
@@ -26,6 +29,7 @@ void WaterFlood(unsigned long currentTime) {
     }
     digitalWrite(ledPin1, ledState);
   }
+
   if (stateDoor == 0) {
     if (currentTime - previousTServo > timeIntervalServoD) {
       previousTServo = currentTime;
@@ -33,10 +37,25 @@ void WaterFlood(unsigned long currentTime) {
       if (servoState == 0) {
         servoState = 90;
         stateDoor = 1;
+        stateB = 1;
       } else {
         servoState = 0;
       }
       myservo.write(servoState);
+    }
+  }
+
+  if (currentTime - previousTBuzzer > timeIntervalBuzzer) {
+    previousTBuzzer = currentTime;
+
+    if (buzzerState == 1) {
+      noTone(buzzerPin);
+      buzzerState = 0;
+    } else {
+      for (int i = 1; i <= 1000; i++) {
+        tone(buzzerPin, 1000);
+      }
+      buzzerState = 1;
     }
   }
 }
@@ -52,6 +71,11 @@ void Ping(unsigned long currentTime) {
   if (distance < 12) {
     WaterFlood(currentTime);
   }
+  else
+  {
+    noTone(buzzerPin);
+  }
+  Serial.println(distance);
   delay(100);
 }
 
@@ -69,32 +93,52 @@ void loop() {
   unsigned long currentTime = millis() + 5000;
   boolean btnState1 = digitalRead(buttonPin1);
   boolean btnState2 = digitalRead(buttonPin2);
-  Serial.println(btnState2);
-  Ping(currentTime);
 
-  if (btnState1 == 1 && btnState2 != 1) {
-    if (stateDoor == 0) {
-      if (currentTime - previousTServo > timeIntervalServoD) {
-        previousTServo = currentTime;
-
-        if (servoState == 0) {
-          servoState = 90;
-          stateDoor = 1;
-        } else {
-          servoState = 0;
-        }
-        myservo.write(servoState);
-      }
-    } else if (stateDoor == 1) {
-      digitalWrite(ledPin2, HIGH);
-      myservo.write(180);
-      delay(5000);
-      myservo.write(90);
-      stateDoor = 0;
+  if (btnState1 == 1) {
+    if (stateB == 0) {
+      stateB = 1;
+    } else if (stateB == 1) {
+      stateB = 2;
+    } else if (stateB == 2) {
+      stateB = 1;
     }
+    delay(300);
+  }
+  if (stateDoor == 0 && stateB == 1) {
+    if (currentTime - previousTServo > timeIntervalServoD) {
+      previousTServo = currentTime;
+      digitalWrite(ledPin1, HIGH);
+      if (servoState == 0) {
+        servoState = 90;
+        stateDoor = 1;
+      } else {
+        servoState = 0;
+      }
+      myservo.write(servoState);
+    }
+  } else if (stateDoor == 1 && stateB == 2) {
+    digitalWrite(ledPin2, HIGH);
+    if (currentTime - previousTServo > timeIntervalServoD) {
+      previousTServo = currentTime;
+      digitalWrite(ledPin2, HIGH);
+
+      if (servoState == 180) {
+        servoState = 90;
+        stateDoor = 0;
+      } else {
+        servoState = 180;
+      }
+      myservo.write(servoState);
+    }
+  }
+  if (btnState2 != 1) {
+    Ping(currentTime);
   }
   if (btnState2 == 1) {
     myservo.write(90);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    noTone(buzzerPin);
   }
   digitalWrite(ledPin1, LOW);
   digitalWrite(ledPin2, LOW);
