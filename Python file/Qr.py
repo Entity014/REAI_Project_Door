3f6f0a86d80e4e6eb097be880e5623fddfc28c47
Entity014 <radlove.012@gmail.com>
2023-03-30 00:40:18 +0700

    dasdsad

--- Python file/Qr.py
+++ Python file/Qr.py
@@ -1,18 +1,20 @@
 import qrcode
 
-dataM = "Master"
+# dataM = "Master"
 # dataS = "Slave"
+data = "https://github.com/Entity014/REAI_Project_Door"
 
 qr = qrcode.QRCode(
         version=1,
         box_size=15,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
         border=1)
-qr.add_data(dataM)
+qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill='black', back_color='white')
 
 # imgM = qrcode.make(dataM)
 # img = qrcode.make(dataS)
-img.save('REAI-Pro/image/Master.png')
-# img.save('REAI-Pro/image/Slave.png')
+# img.save('Python file/image/Master.png')
+# img.save('Python file/image/Slave.png')
+img.save('Python file/image/Data.png')
